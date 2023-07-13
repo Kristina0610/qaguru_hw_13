46d5eb97bfb285e2ee48a9a1d2049323fddd6d41
Kristina0610 <gaevskaya-2011@mail.ru>
2023-07-13 22:25:27 +0300

    added SystemPropertiesTest.java

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -5,7 +5,6 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
